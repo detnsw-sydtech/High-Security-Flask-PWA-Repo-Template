83b477d687861725d6ceff60e76dbfa1e57c9e90
David Hawkes <david.hawkes11@det.nsw.edu.au>
2026-01-16 18:09:00 +1100

    Update __init__.py

--- src/app/db/__init__.py
+++ src/app/db/__init__.py
@@ -1,5 +1,8 @@
 # src/app/db/__init__.py
-
+from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from . import routes # noqa: E402, F401
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 db = SQLAlchemy()
